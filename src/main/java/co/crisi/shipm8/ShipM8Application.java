371790d4c11edec1b97ed7ef42d0087f413a9d72
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-09 09:49:04 -0500

    Start

---
+++ src/main/java/co/crisi/shipm8/ShipM8Application.java
@@ -0,0 +1,13 @@
+package co.crisi.shipm8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipM8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipM8Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/crisi/shipm8/ShipM8ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.crisi.shipm8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipM8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
