4bba9717b016685faa5dd1b5607b9c79c35e7ebe
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-09 16:11:26 -0500

    Implemented validators

--- domain/src/main/java/co/crisi/shipm8/domain/data/Product.java
+++ domain/src/main/java/co/crisi/shipm8/domain/data/Product.java
@@ -2,10 +2,27 @@ package co.crisi.shipm8.domain.data;
 
 import co.crisi.shipm8.domain.IProduct;
 import co.crisi.shipm8.domain.data.Discount;
+import co.crisi.shipm8.domain.validator.decorator.NonNegativeNumberValidatorDecorator;
+import co.crisi.shipm8.domain.validator.decorator.NonNullValidatorDecorator;
+import co.crisi.shipm8.domain.validator.decorator.Validator;
 import java.util.List;
 
 public record Product(Long id, Long productId, Integer quantity, Double pricePerUnit, Double totalPrice,
                       List<Discount> discounts, Double tax) implements IProduct {
+
+    public Product(Long id, Long productId, Integer quantity, Double pricePerUnit, Double totalPrice,
+            List<Discount> discounts, Double tax) {
+        var validator = new Validator();
+        var nonNullValidator = new NonNullValidatorDecorator(validator);
+        var nonNegativeNumberValidator = new NonNegativeNumberValidatorDecorator(nonNullValidator);
+        this.id = id;
+        this.productId = nonNullValidator.validate(productId, "product id");
+        this.quantity = nonNegativeNumberValidator.validate(quantity, "quantity");
+        this.pricePerUnit = nonNegativeNumberValidator.validate(pricePerUnit, "price per unit");
+        this.totalPrice = nonNegativeNumberValidator.validate(totalPrice, "total price");
+        this.discounts = discounts;
+        this.tax = nonNegativeNumberValidator.validate(tax, "tax");
+    }
 
     @Override
     public Long getId() {
