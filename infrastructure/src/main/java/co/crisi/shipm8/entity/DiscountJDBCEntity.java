876f7f5643d8b1971214a80d0edf7915f974c62b
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-10 09:22:33 -0500

    added discount jdbc entity

--- infrastructure/src/main/java/co/crisi/shipm8/entity/DiscountJDBCEntity.java
+++ infrastructure/src/main/java/co/crisi/shipm8/entity/DiscountJDBCEntity.java
@@ -1,17 +1,93 @@
 package co.crisi.shipm8.entity;
 
 
+import co.crisi.shipm8.domain.IDiscount;
+import co.crisi.shipm8.domain.data.Applicability;
+import co.crisi.shipm8.domain.data.DiscountType;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class DiscountJDBCEntity {
+public class DiscountJDBCEntity implements Serializable, IDiscount {
 
     @Id
     private Long id;
+
+    private String name;
+
+    private Double value;
+
+    private DiscountType type;
+
+    @Column("start_date")
+    private LocalDate startDate;
+
+    @Column("end_date")
+    private LocalDate endDate;
+
+    private Applicability applicability;
+
+    @Column("minimum_order_amount")
+    private Integer minimumOrderAmount;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Double getValue() {
+        return value;
+    }
+
+    @Override
+    public DiscountType getType() {
+        return type;
+    }
+
+    @Override
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    @Override
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public Applicability getApplicability() {
+        return applicability;
+    }
+
+    @Override
+    public Integer getMinimumOrderAmount() {
+        return minimumOrderAmount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        DiscountJDBCEntity that = (DiscountJDBCEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 }
