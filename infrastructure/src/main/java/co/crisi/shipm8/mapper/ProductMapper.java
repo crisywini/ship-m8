38d92f2a6bac21c6cf2b02447c6167ac0c012219
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-10 11:57:28 -0500

    added product mapper

---
+++ infrastructure/src/main/java/co/crisi/shipm8/mapper/ProductMapper.java
@@ -0,0 +1,12 @@
+package co.crisi.shipm8.mapper;
+
+import co.crisi.shipm8.domain.IProduct;
+import co.crisi.shipm8.entity.ProductJDBCEntity;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface ProductMapper {
+
+    ProductJDBCEntity map(IProduct product);
+
+}
