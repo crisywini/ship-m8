2817b6a6811c13190b801c89775689b1a3a7d310
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-11 11:54:25 -0500

    ref

--- application/src/main/java/co/crisi/shipm8/controller/OrderController.java
+++ application/src/main/java/co/crisi/shipm8/controller/OrderController.java
@@ -1,5 +1,17 @@
 package co.crisi.shipm8.controller;
 
+import co.crisi.shipm8.port.api.IOrderServicePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final IOrderServicePort servicePort;
+
+
 
 }
