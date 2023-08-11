81464f6d5aaa0c3d6a960aad336f9fc1c1dca41c
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-08-11 11:34:14 -0500

    added delete by id service

--- application/src/main/java/co/crisi/shipm8/controller/AddressController.java
+++ application/src/main/java/co/crisi/shipm8/controller/AddressController.java
@@ -10,6 +10,7 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,7 +58,11 @@ public class AddressController {
         return ResponseEntity.ok(mapper.map(updated));
     }
 
-    
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable(name = "id") Long id){
+        servicePort.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
