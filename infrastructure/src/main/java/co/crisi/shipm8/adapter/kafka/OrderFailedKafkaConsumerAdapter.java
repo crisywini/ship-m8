2a4d8e9b5ba710193824a929961cb54483ac26ea
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-09-04 08:45:42 -0500

    Implemented order failed listener

--- infrastructure/src/main/java/co/crisi/shipm8/adapter/kafka/OrderFailedKafkaConsumerAdapter.java
+++ infrastructure/src/main/java/co/crisi/shipm8/adapter/kafka/OrderFailedKafkaConsumerAdapter.java
@@ -1,8 +1,14 @@
 package co.crisi.shipm8.adapter.kafka;
 
+import co.crisi.shipm8.domain.IProduct;
+import co.crisi.shipm8.domain.data.Order;
+import co.crisi.shipm8.domain.data.OrderStatus;
+import co.crisi.shipm8.domain.data.PaymentStatus;
 import co.crisi.shipm8.domain.message.OrderFailed;
 import co.crisi.shipm8.domain.message.Topics;
+import co.crisi.shipm8.port.api.IOrderServicePort;
 import co.crisi.shipm8.port.spi.message.IReceiveMessagePort;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,11 +20,33 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class OrderFailedKafkaConsumerAdapter implements IReceiveMessagePort<OrderFailed> {
 
+    private final IOrderServicePort servicePort;
+
     @Transactional
     @KafkaListener(id = "id", topics = Topics.EXCEPTION)
     @Override
     public void listen(OrderFailed message) {
         log.info("Processing order failed message! {} ", message.getFailedOrder());
+
+        var order = servicePort.getById(message.getFailedOrder().orderId());
+        var newOrderInfo = Order.builder()
+                .id(order.getId())
+                .orderDate(order.getOrderDate())
+                .orderStatus(OrderStatus.FAILED)
+                .totalPrice(order.getTotalPrice())
+                .shippingAddress(order.getShippingAddress())
+                .billingAddress(order.getBillingAddress())
+                .paymentMethod(order.getPaymentMethod())
+                .paymentStatus(PaymentStatus.FAILED)
+                .shippingMethod(order.getShippingMethod())
+                .orderNotes(order.getOrderNotes())
+                .cancellationReason(message.getFailedOrder().errorMessage())
+                .orderCompletionDate(null)
+                .products((List<IProduct>) order.getProducts())
+                .shopper(order.getShopper())
+                .build();
+        var orderUpdated = servicePort.update(message.getFailedOrder().orderId(), newOrderInfo);
+        log.debug("Order failed with id {}", orderUpdated.getId());
     }
 
 }
