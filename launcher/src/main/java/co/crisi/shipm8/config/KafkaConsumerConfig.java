77dcf5fe9215a93747a9155e8f05f1abd0adaf94
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-09-05 17:21:42 -0500

    Added banner and local kafka config

--- launcher/src/main/java/co/crisi/shipm8/config/KafkaConsumerConfig.java
+++ launcher/src/main/java/co/crisi/shipm8/config/KafkaConsumerConfig.java
@@ -34,12 +34,12 @@ public class KafkaConsumerConfig {
         config.put(
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
                 OrderFailedDeserializer.class);
-
+        /*
         config.put("sasl.jaas.config",
                 "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"51x8WVctckIWnPCu5yIn5t\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiI1MXg4V1ZjdGNrSVduUEN1NXlJbjV0Iiwib3JnYW5pemF0aW9uSWQiOjc2MDA0LCJ1c2VySWQiOjg4NDQzLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiI4NmJkZDYwZi1jMDZhLTQ3ODUtODNhMy0yNzkzODQ1ZmQ2ZGUifX0.nLaK4Z_rSwGA-aPO8SU2JYfJTpLxUqvAe3sev5yufMA\";");
         config.put("sasl.mechanism", "PLAIN");
         config.put("security.protocol", "SASL_SSL");
-
+        */
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
--- launcher/src/main/java/co/crisi/shipm8/config/KafkaProducerConfig.java
+++ launcher/src/main/java/co/crisi/shipm8/config/KafkaProducerConfig.java
@@ -28,10 +28,10 @@ public class KafkaProducerConfig {
                 StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 OrderProcessedSerializer.class);
-        props.put("sasl.jaas.config",
+        /*props.put("sasl.jaas.config",
                 "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"51x8WVctckIWnPCu5yIn5t\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiI1MXg4V1ZjdGNrSVduUEN1NXlJbjV0Iiwib3JnYW5pemF0aW9uSWQiOjc2MDA0LCJ1c2VySWQiOjg4NDQzLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiI4NmJkZDYwZi1jMDZhLTQ3ODUtODNhMy0yNzkzODQ1ZmQ2ZGUifX0.nLaK4Z_rSwGA-aPO8SU2JYfJTpLxUqvAe3sev5yufMA\";");
         props.put("sasl.mechanism", "PLAIN");
-        props.put("security.protocol", "SASL_SSL");
+        props.put("security.protocol", "SASL_SSL");*/
         return props;
     }
 
